723ff38a18a9aca10a73b5399f1fdf2b28c8c33c
ajay <ap1450ajay@gmail.com>
2024-12-03 19:10:18 +0530

    Add project files.

---
+++ ProductData/ProductData/Controllers/ProductDataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductData.Models;
+using System.Data.Common;
+
+namespace ProductData.Controllers
+{
+    public class ProductDataController : Controller
+    {
+        public IActionResult GetProductDetails(int pg = 1)
+        {
+            // data from dbcontext  dbdata
+
+            List<ProductDetails> dbdata = new List<ProductDetails>
+            {
+                      new ProductDetails
+                      {
+                          ProductId = 1,
+                          ProductName = "Patanjali",
+                          CategoryId = 1,
+                          CategoryName = "HighQuality"
+                      }
+             };
+
+            new List<ProductDetails>();
+            int pageSize = 10;
+            int productcount = dbdata.Count(); 
+            var pager = new PageIn(productcount, pg, pageSize);
+            int pddata = (pg - 1) * pageSize;
+            var data = dbdata.Skip(pddata).Take(pager.PageSize).ToList();
+            this.ViewBag.PageIn = pager;
+            return View(data);
+        }
+    }
+}
---
+++ ProductData/ProductData/Models/ProductDetails.cs
@@ -0,0 +1,64 @@
+﻿namespace ProductData.Models
+{
+    public class ProductDetails
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+
+    //public class PaginatedList<T>
+    //{
+    //    public IEnumerable<T> Items { get; set; }
+    //    public int PageNumber { get; set; }
+    //    public int TotalPages { get; set; }
+    //    public bool HasPreviousPage => PageNumber > 1;
+    //    public bool HasNextPage => PageNumber < TotalPages;
+    //}
+
+    public class PageIn
+    {
+        public int Totalitems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPage { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public PageIn(int totalitems, int page, int pageSize = 10)
+        {
+            int totalPage = (int)Math.Ceiling((decimal)totalitems /(decimal)pageSize);
+            int currentPage = page;
+            int startPage = currentPage - 5;
+            int endPage = currentPage - 4;
+
+            if(startPage <= 0)
+            {
+                endPage = endPage - (startPage - 1);    
+                startPage = +1;
+
+            }
+            if (endPage <= 0)             
+            {
+                endPage = totalPage;
+                if(endPage > 10)
+                {
+                    startPage = endPage - 9;
+                }
+            }
+
+            Totalitems = totalPage;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalPage = totalPage;
+            StartPage = startPage;
+            EndPage = endPage;
+
+        }
+
+    }
+
+    
+
+}
